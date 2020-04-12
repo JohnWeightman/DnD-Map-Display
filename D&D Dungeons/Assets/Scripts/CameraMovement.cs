a69a05f5b3f74e9666f509091fd09aa5bba221ac
John Weightman <JohnCWeightman>
2020-04-12 12:30:20 +0100

    Initiliaze Repository
    - Added current project progress to github

---
+++ D&D Dungeons/Assets/Scripts/CameraMovement.cs
@@ -0,0 +1,44 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraMovement : MonoBehaviour
+{
+    float XCameraAxis;
+    float ZCameraAxis;
+    public float CameraSpeed;
+
+    bool Up;
+    bool Down;
+    bool Left;
+    bool Right;
+
+    void Update()
+    {
+        CamaraMovement();
+    }
+
+    void CamaraMovement()
+    {
+        if (Input.GetKey(KeyCode.UpArrow) || Input.GetKey(KeyCode.W))
+        {
+            transform.Translate(new Vector3(0, 0, CameraSpeed * Time.deltaTime));
+            transform.position = new Vector3(transform.position.x, 45f, transform.position.z);
+        }
+        else if (Input.GetKey(KeyCode.DownArrow) || Input.GetKey(KeyCode.S))
+        {
+            transform.Translate(new Vector3(0, 0, -CameraSpeed * Time.deltaTime));
+            transform.position = new Vector3(transform.position.x, 45f, transform.position.z);
+        }
+        if (Input.GetKey(KeyCode.LeftArrow) || Input.GetKey(KeyCode.A))
+        {
+            transform.Translate(new Vector3(-CameraSpeed * Time.deltaTime, 0, 0));
+            transform.position = new Vector3(transform.position.x, 45f, transform.position.z);
+        }
+        else if (Input.GetKey(KeyCode.RightArrow) || Input.GetKey(KeyCode.D))
+        {
+            transform.Translate(new Vector3(CameraSpeed * Time.deltaTime, 0, 0));
+            transform.position = new Vector3(transform.position.x, 45f, transform.position.z);
+        }
+    }
+}
---
+++ D&D Dungeons/Assets/Scripts/SaveLoadMap.cs
@@ -0,0 +1,82 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using System.Xml;
+
+public class SaveLoadMap : MonoBehaviour
+{
+    [Header("Script Variables")]
+    public bool SaveMap = false;
+    public string MapName;
+    public GameObject Map;
+
+    [Header("Prefabs")]
+    public GameObject Tree;
+    public GameObject Grass;
+    public GameObject DirtRoad;
+
+    void Start()
+    {
+        if (SaveMap)
+            SaveMapFile();
+        else
+            LoadMapFile();
+    }
+
+    void SaveMapFile()
+    {
+        XmlWriter XML = XmlWriter.Create("Assets\\Maps\\" + MapName + ".xml");
+        XML.WriteStartDocument();
+        XML.WriteStartElement("Map");
+        XML.WriteStartElement("Ground");
+        for (int x = 0; x < Map.transform.GetChild(0).transform.childCount; x++)
+        {
+            Map.transform.GetChild(0).transform.GetChild(x).name = CheckName(Map.transform.GetChild(0).transform.GetChild(x));
+            XML.WriteStartElement(Map.transform.GetChild(0).GetChild(x).gameObject.name);
+            XML.WriteAttributeString("Name", Map.transform.GetChild(0).GetChild(x).gameObject.name);
+            XML.WriteAttributeString("XPos", Convert.ToString(Map.transform.GetChild(0).GetChild(x).transform.position.x));
+            XML.WriteAttributeString("YPos", Convert.ToString(Map.transform.GetChild(0).GetChild(x).transform.position.y));
+            XML.WriteAttributeString("ZPos", Convert.ToString(Map.transform.GetChild(0).GetChild(x).transform.position.z));
+            XML.WriteEndElement();
+        }
+        XML.WriteEndElement();
+        XML.WriteStartElement("Items");
+        for (int x = 0; x < Map.transform.GetChild(1).transform.childCount; x++)
+        {
+            Map.transform.GetChild(1).transform.GetChild(x).name = CheckName(Map.transform.GetChild(1).transform.GetChild(x));
+            XML.WriteStartElement(Map.transform.GetChild(1).GetChild(x).gameObject.name);
+            XML.WriteAttributeString("Name", Map.transform.GetChild(1).GetChild(x).gameObject.name);
+            XML.WriteAttributeString("XPos", Convert.ToString(Map.transform.GetChild(1).GetChild(x).transform.position.x));
+            XML.WriteAttributeString("YPos", Convert.ToString(Map.transform.GetChild(1).GetChild(x).transform.position.y));
+            XML.WriteAttributeString("ZPos", Convert.ToString(Map.transform.GetChild(1).GetChild(x).transform.position.z));
+            XML.WriteEndElement();
+        }
+        XML.WriteEndElement();
+        XML.WriteEndElement();
+        XML.WriteEndDocument();
+        XML.Close();
+    }
+
+    string CheckName(Transform Tile)
+    {
+        string TileName = Tile.name;
+        TileName = TileName.Replace("(", "");
+        TileName = TileName.Replace(")", "");
+        TileName = TileName.Replace(" ", "");
+        return TileName;
+    }
+
+    void LoadMapFile()
+    {
+        XmlReader XML = XmlReader.Create("Assets\\Maps\\" + MapName + ".xml");
+        while(XML.Read() == true)
+        {
+            if ((XML.NodeType == XmlNodeType.Element) && (XML.Depth == 2))
+            {
+                Debug.Log(XML.GetAttribute("Name"));
+                Instantiate(Resources.Load<GameObject>("Prefabs\\" + XML.GetAttribute("Name")), new Vector3((float) Convert.ToDouble(XML.GetAttribute("XPos")), (float)Convert.ToDouble(XML.GetAttribute("YPos")), (float)Convert.ToDouble(XML.GetAttribute("ZPos"))), Quaternion.identity);
+            }
+        }
+    }
+}
